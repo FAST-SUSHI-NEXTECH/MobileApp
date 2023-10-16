9f8009ebe03292f705b844f649ec56b2eeaa5560
BlackStorM13 <esposito.raphael1@gmail.com>
2023-10-16 10:54:02 +0200

    AppBar fonctionnel

--- lib/main.dart
+++ lib/main.dart
@@ -19,15 +19,18 @@ class LoginPage extends StatelessWidget {
               Container(
                 padding: const EdgeInsets.only(right: 0),
                 child: const Text("FastSushi",
-                style: TextStyle(fontFamily: 'Comfortaa')
+                style: TextStyle(fontFamily: 'Comfortaa',
+                                 fontSize: 30),
                   ),
               ),
-              Image.asset("assets/images/logo_blanc.png"),
+              Image.asset("assets/images/logo_blanc.png",
+              width: 100,),
             ],
           ),
         backgroundColor: const Color(0xFF000000),
         toolbarHeight: 125,
       ),
+      // body: ,
     );
   }
 }
