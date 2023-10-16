20055164b5090a78d6339e5384252b4278edd1ee
BlackStorM13 <esposito.raphael1@gmail.com>
2023-10-16 11:06:11 +0200

    creation d'une page login pour mieux de lisibilité

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:dev/pages/LoginPage.dart';
 import 'package:flutter/material.dart';
+
 
 void main() {
   runApp(const MaterialApp(
@@ -6,32 +8,16 @@ void main() {
   ));
 }
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                padding: const EdgeInsets.only(right: 0),
-                child: const Text("FastSushi",
-                style: TextStyle(fontFamily: 'Comfortaa',
-                                 fontSize: 30),
-                  ),
-              ),
-              Image.asset("assets/images/logo_blanc.png",
-              width: 100,),
-            ],
-          ),
-        backgroundColor: const Color(0xFF000000),
-        toolbarHeight: 125,
-      ),
-      // body: ,
-    );
+    return const Placeholder();
   }
 }
+
+
+
+
 
