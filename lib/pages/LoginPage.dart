c2d36fa0e9f5b3d6af42d08da8039f2d9f675fe0
BlackStorM13 <esposito.raphael1@gmail.com>
2023-10-16 11:58:16 +0200

    champ ID et MDP créer

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -29,7 +29,40 @@ class _LoginPageState extends State<LoginPage> {
         backgroundColor: const Color(0xFF000000),
         toolbarHeight: 125,
       ),
-      // body:,
+      body: Container(
+        margin: EdgeInsets.only(right: 150, left: 150),
+        child: Form(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Identifiant",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                    )
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Mot de passe",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide(
+                        color: Colors.black,
+                      )
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+        ),
+      ),
     );
   }
 }
