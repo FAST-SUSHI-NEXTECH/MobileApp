bc890321862e853d3aa732ae5302b45a6b0b7cd1
BlackStorM13 <esposito.raphael1@gmail.com>
2023-10-24 14:34:31 +0200

    creation de la page detail des commandes

--- lib/pages/DetailCommandPage.dart
+++ lib/pages/DetailCommandPage.dart
@@ -1,31 +1,61 @@
 import 'package:flutter/material.dart';
 
-class DetailCommandePage extends StatefulWidget {
-  const DetailCommandePage({super.key});
+class DetailCommandPage extends StatefulWidget {
+  const DetailCommandPage({super.key});
 
   @override
-  State<DetailCommandePage> createState() => _DetailCommandePageState();
+  State<DetailCommandPage> createState() => _DetailCommandePageState();
 }
 
-class _DetailCommandePageState extends State<DetailCommandePage> {
+class _DetailCommandePageState extends State<DetailCommandPage> {
   @override
   Widget build(BuildContext context) {
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    double spacing;
+    if (mediaQueryData.orientation == Orientation.portrait) {
+      spacing = 150;
+    } else {
+      spacing = 300;
+    }
+
     return Scaffold(
-        appBar: AppBar(
-          title: const Wrap(
-            spacing: 150,
-            children: [
-              Text("Clovis Delacroix"),
-              Text("N°6"),
-              Text("15h56"),
-              Text("50,00 €"),
-            ],
-          ),
-          backgroundColor: Colors.black,
-          toolbarHeight: 85,
+      appBar: AppBar(
+        title: Wrap(
+          spacing: spacing,
+          children: const [
+            Text("Clovis Delacroix"),
+            Text("N°6"),
+            Text("15h56"),
+            Text("50,00 €"),
+          ],
         ),
-        body: ElevatedButton(
-            onPressed: () {}, child:
-            const Text("Revenir")));
+        backgroundColor: Colors.black,
+        toolbarHeight: 85,
+      ),
+      body: Row(
+        children: [
+          Container(
+            alignment: AlignmentDirectional.bottomStart,
+            margin: const EdgeInsets.only(left: 50, bottom: 50),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(fixedSize: const Size(175, 75)),
+              child: const Text("Revenir"),
+            ),
+          ),
+          SizedBox(width: 800),
+          Container(
+            alignment: AlignmentDirectional.bottomEnd,
+            margin: const EdgeInsets.only(right: 50, bottom: 50),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(fixedSize: const Size(175, 75)),
+              child: const Text("Terminer"),
+            ),
+          ),
+        ],
+      ),
+
+    );
   }
 }
