c3c1fa630470d826c470eb9f727cbda4064ad5e4
ThomasMoulins <13thomasmoulins@gmail.com>
2023-12-19 14:10:21 +0100

    Update .gitignore
    Ajout de l'heure de la commande + changement de la méthode ne renvois plus seulement id mais une liste avec idOrder + time

--- lib/classes/OrderClass.dart
+++ lib/classes/OrderClass.dart
@@ -3,8 +3,16 @@ import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:dev/conf.dart';
 
+// Classe pour représenter une commande avec son ID et l'heure
+class Order {
+  final int? idOrder;
+  final String? time;
+
+  Order({this.idOrder, this.time});
+}
+
 class Orders {
-  Future<List<int?>> fetchIdOrders() async {
+  Future<List<Order>> fetchIdOrders() async {
     try {
       final response = await http.get(Uri.parse("$ipApi/order"));
 
@@ -12,18 +20,30 @@ class Orders {
         if (kDebugMode) {
           print('Réponse: ${response.body}');
         }
+
         List<dynamic> ordersList = json.decode(response.body);
-        List<int?> idOrders = [];
+        List<Order> orders = [];
 
         for (var order in ordersList) {
+
+          int? idOrder;
           if (order['id_order'] != null) {
-            int idOrder = int.parse(order['id_order'].toString());
-            idOrders.add(idOrder);
+            idOrder = int.parse(order['id_order'].toString());
           } else {
-            idOrders.add(null);
+            idOrder = null;
           }
+
+          String? time;
+          if (order['date'] != null) {
+            String dateTime = order['date'];
+            DateTime parsedDateTime = DateTime.parse(dateTime).toLocal();
+            time = "${parsedDateTime.hour.toString().padLeft(2, '0')}:${parsedDateTime.minute.toString().padLeft(2, '0')}";
+            // Formate l'heure au format "HH:mm"
+          }
+
+          orders.add(Order(idOrder: idOrder, time: time));
         }
-        return idOrders;
+        return orders;
       } else {
         if (kDebugMode) {
           print('Erreur: ${response.statusCode}');
