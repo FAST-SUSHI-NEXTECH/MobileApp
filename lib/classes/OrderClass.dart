9b464b293021333ce26581f632f84e985d7739ef
ThomasMoulins <13thomasmoulins@gmail.com>
2023-12-19 11:27:39 +0100

    Update OrderClass.dart
    Ajout d'une méthode pour récupérer id_order

--- lib/classes/OrderClass.dart
+++ lib/classes/OrderClass.dart
@@ -4,4 +4,19 @@ import 'package:http/http.dart' as http;
 import 'package:dev/conf.dart';
 
 class Orders {
+  Future<List<int?>> fetchIdOrders() async {
+      final response = await http.get(Uri.parse("$ipApi/order"));
+        List<dynamic> ordersList = json.decode(response.body);
+        List<int?> idOrders = [];
+
+        for (var order in ordersList) {
+          if (order['id_order'] != null) {
+            int idOrder = int.parse(order['id_order'].toString());
+            idOrders.add(idOrder);
+          } else {
+            idOrders.add(null);
+          }
+        }
+        return idOrders;
+  }
 }
