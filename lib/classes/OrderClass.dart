2ee59e507862ea8cc58faf14cbccda9a8ede4c77
ThomasMoulins <13thomasmoulins@gmail.com>
2024-01-12 14:34:24 +0100

    Update OrderClass.dart
    ajout d'une méthode pour la page /pages/HistoryPage.dart
    récupération des commandes à l'état 3

--- lib/classes/OrderClass.dart
+++ lib/classes/OrderClass.dart
@@ -79,7 +79,61 @@ class Orders {
     }
   }
 
+  Future<List<Order>> fetchOldOrders() async {
+    try {
+      final response = await http.get(
+        Uri.parse("$ipApi/order"),
+        headers: {
+          'Authorization': 'Bearer $tokenApi',
+          'Accept': 'application/json',
+        },
+      );
 
+      if (response.statusCode == 200) {
+        if (kDebugMode) {
+          print('Réponse: ${response.body}');
+        }
+
+        List<dynamic> ordersList = json.decode(response.body);
+        List<Order> orders = [];
+
+        for (var order in ordersList) {
+          if (order['order_state'] == 3) {
+            int? idOrder;
+            if (order['id_order'] != null) {
+              idOrder = int.parse(order['id_order'].toString());
+            } else {
+              idOrder = null;
+            }
+
+            String? time;
+            if (order['date'] != null) {
+              String dateTime = order['date'];
+              DateTime parsedDateTime = DateTime.parse(dateTime).toLocal();
+              time = "${parsedDateTime.hour.toString().padLeft(
+                  2, '0')}:${parsedDateTime.minute.toString().padLeft(2, '0')}";
+              // Formate l'heure au format "HH:mm"
+            }
+
+            int? idPicker;
+            if (order['id_picker'] != null) {
+              idPicker = int.parse(order['id_picker'].toString());
+            } else {
+              idPicker = null;
+            }
+
+            int? orderState;
+            if (order['order_state'] != null) {
+              orderState = int.parse(order['order_state'].toString());
+            } else {
+              orderState = null;
+            }
+
+            orders.add(Order(idOrder: idOrder,
+                time: time,
+                idPicker: idPicker,
+                orderState: orderState));
+          }
         }
         return orders;
       } else {
