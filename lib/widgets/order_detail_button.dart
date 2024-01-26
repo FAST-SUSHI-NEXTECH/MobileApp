2d97fd648930d8c15f458c41e8eb22f4d2f02843
ThomasMoulins <13thomasmoulins@gmail.com>
2024-01-26 10:58:56 +0100

    add widget detail button of orders
    copie du widgets du bouton servant de modification de l'état de la commande avec quelques modification

--- lib/widgets/order_detail_button.dart
+++ lib/widgets/order_detail_button.dart
@@ -1,1 +1,82 @@
 import 'package:flutter/material.dart';
+import '../classes/OrderClass.dart';
+import '../pages/DetailOrderPage.dart';
+
+class OrderDetailButton extends StatelessWidget {
+  final int? orderState;
+  final Order order;
+  final Function(int?, int?) onUpdateOrderState;
+
+  const OrderDetailButton({
+    Key? key,
+    this.orderState,
+    required this.order,
+    required this.onUpdateOrderState
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton.icon(
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(
+                _getButtonColor(orderState)
+            )
+        ),
+        onPressed: _getButtonOnPressed(context, orderState),
+        label: Text(_getButtonLabel(orderState),
+          style: const TextStyle(
+              fontFamily: 'Comfortaa',
+              fontSize: 17
+          ),
+        ),
+        icon: Icon(_getButtonIcon(orderState)),
+      ),
+    );
+  }
+
+  Color _getButtonColor(int? state) {
+    switch (state) {
+      case 1 || 2:
+        return Colors.green;
+      default:
+        return Colors.red;
+    }
+  }
+
+  Future<void> Function()? _getButtonOnPressed(BuildContext context, int? state) {
+    switch (state) {
+      case 1 || 2:
+        return
+          () async {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => DetailOrderPage(orderId: order.idOrder)
+              ),
+            );
+          };
+      default:
+        return null;
+    }
+  }
+
+  String _getButtonLabel(int? state) {
+    switch (state) {
+      case 1 || 2:
+        return "Détail";
+      default:
+        return "ERREUR";
+    }
+  }
+
+  IconData _getButtonIcon(int? state) {
+    switch (state) {
+      case 1 || 2:
+        return Icons.list;
+      default:
+        return Icons.warning;
+    }
+  }
+}
