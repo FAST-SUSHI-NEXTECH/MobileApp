715f5f995febe4a32cf08922f9f87f759e9d764d
ThomasMoulins <13thomasmoulins@gmail.com>
2024-02-06 10:09:25 +0100

    ADD timezone to main for Appbar
    Ajout de la localisation (Paris) pour être sur le bon fuseau horaire

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:dev/pages/LoginPage.dart';
 import 'package:flutter/material.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 
 void main() {
+  tz.initializeTimeZones(); // Initialise les données de fuseau horaire
+  tz.setLocalLocation(tz.getLocation('Europe/Paris')); // Définir le fuseau horaire local
   runApp(const MaterialApp(
     home: LoginPage(),
   ));
