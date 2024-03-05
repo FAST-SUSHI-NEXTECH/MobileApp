7f39ea78023eee2dfdc72ae27ae5869792d468f3
ThomasMoulins <13thomasmoulins@gmail.com>
2024-03-05 14:30:53 +0100

    ADD usernamePicker, idOrder, time, price
    Ajout des valeurs de la bdd

--- lib/widgets/order_detail_appbar.dart
+++ lib/widgets/order_detail_appbar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:dev/conf.dart';
-import 'package:dev/classes/OrderClass.dart'; // Remplacez par le chemin correct vers votre fichier OrderClass.dart
+import 'package:dev/classes/OrderClass.dart';
+import '../classes/PickerClass.dart';
 
 class OrderAppBar extends StatelessWidget implements PreferredSizeWidget {
   final Order order;
@@ -9,18 +10,30 @@ class OrderAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
+    Orders ordersClass = Orders();
+
     return AppBar(
         automaticallyImplyLeading: false, // Supprime le bouton pour revenir en arrière
         backgroundColor: const Color(0xFF000000),
         title: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+
               Flexible(
                   flex: 1,
                   child: Container(
                       alignment: Alignment.center,
                       margin: const EdgeInsets.only(left: 50),
-                      child: Text('${order.orderContentPlate}', style: TextStyle(fontFamily: Conf.police, fontSize: 25),)
+                    child: FutureBuilder<String>(
+                      future: Pickers().fetchPickerUsername(order.idPicker),
+                      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                        if (snapshot.hasError) {
+                          return Text("Erreur", style: TextStyle(fontFamily: Conf.police, fontSize: 25));
+                        } else {
+                          return Text('${snapshot.data}', style: TextStyle(fontFamily: Conf.police, fontSize: 25));
+                        }
+                      }
+                    )
                   )
               ),
               Flexible(
@@ -28,7 +41,7 @@ class OrderAppBar extends StatelessWidget implements PreferredSizeWidget {
                   child: Container(
                       alignment: Alignment.center,
                       margin: const EdgeInsets.only(left: 50),
-                      child: Text('${order.orderContentAppetizer}', style: TextStyle(fontFamily: Conf.police, fontSize: 25),)
+                      child: Text('N°${order.idOrder}', style: TextStyle(fontFamily: Conf.police, fontSize: 25),)
                   )
               ),
               Flexible(
@@ -36,7 +49,7 @@ class OrderAppBar extends StatelessWidget implements PreferredSizeWidget {
                   child: Container(
                       alignment: Alignment.center,
                       margin: const EdgeInsets.only(left: 50),
-                      child: Text('${order.orderContentDessert}', style: TextStyle(fontFamily: Conf.police, fontSize: 25),)
+                      child: Text(order.time ?? 'Temps inconnu', style: TextStyle(fontFamily: Conf.police, fontSize: 25),)
                   )
               ),
               Flexible(
@@ -44,15 +57,17 @@ class OrderAppBar extends StatelessWidget implements PreferredSizeWidget {
                   child: Container(
                       alignment: Alignment.center,
                       margin: const EdgeInsets.only(left: 50),
-                      child: Text('Appetizer: ${order.orderContentAppetizer}\n', style: TextStyle(fontFamily: Conf.police, fontSize: 25),)
-                  )
-              ),
-              Flexible(
-                  flex: 1,
-                  child: Container(
-                      alignment: Alignment.center,
-                      margin: const EdgeInsets.only(left: 50),
-                      child: Text('Appetizer: ${order.orderContentAppetizer}\n', style: TextStyle(fontFamily: Conf.police, fontSize: 25),)
+                      child: FutureBuilder<double>(
+                        future: ordersClass.fetchOrderPrice(order.idOrder), // Appelle la fonction fetchOrderPrice
+                        builder: (context, snapshot) {
+                          if (snapshot.hasError) {
+                            return Text("Erreur", style: TextStyle(fontFamily: Conf.police, fontSize: 25)); // Gestion des erreurs
+                          } else {
+                            // Affiche le prix si la donnée est disponible
+                            return Text('${snapshot.data?.toStringAsFixed(2)} €', style: TextStyle(fontFamily: Conf.police, fontSize: 25));
+                          }
+                        },
+                      )
                   )
               )
             ]
